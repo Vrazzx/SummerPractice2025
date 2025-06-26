865d872a026b7e298e5a9c9e5ad2913db6700efd
Vrazzx <ivan.vraz@gmail.com>
2025-06-26 13:31:50 +0600

    Создан тест по образцу из задания. Написана реализаиця функции-расширения isPalindrome

--- task01/Class1.cs
+++ task01/Class1.cs
@@ -1,6 +1,19 @@
-﻿namespace task01;
+﻿using System;
+using System.Linq;
 
-public class Class1
+public static class StringExtensions
 {
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return false;
 
+        var cleanedString = input.ToLower()
+            .Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+            .ToArray();
+
+        var reversedString = cleanedString.Reverse().ToArray();
+        
+        return new string(cleanedString).Equals(new string(reversedString));
+    }
 }
