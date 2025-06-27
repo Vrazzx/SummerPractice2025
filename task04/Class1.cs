2eb86ed6efc5fae2bab73f77543bde17f1502ed4
Vrazzx <ivan.vraz@gmail.com>
2025-06-27 18:32:35 +0600

    созданы два класса Cruiser и Fighter, реализован интерфейс, дописаны тесты

--- task04/Class1.cs
+++ task04/Class1.cs
@@ -1,6 +1,49 @@
 ﻿namespace task04;
 
-public class Class1
+public interface ISpaceship
 {
+    void MoveForward();      // Движение вперед
+    void Rotate(int angle);  // Поворот на угол (градусы)
+    void Fire();             // Выстрел ракетой
+    int Speed { get; }       // Скорость корабля
+    int FirePower { get; }   // Мощность выстрела
+}
+
+public class Cruiser : ISpaceship
+{
+    public int Speed { get; } = 50;
+    public int FirePower { get; } = 100;
+
+    public void MoveForward()
+    {
+        System.Console.WriteLine($"Крейсер движется впред! его скорость: {Speed}");
+    }
+    public void Rotate(int angle)
+    {
+        System.Console.WriteLine($"Крейсер поворачивается на {angle} градусов!");
+    }
+    public void Fire()
+    {
+        System.Console.WriteLine($"Крейсер атакует! мощность его выстрела: {FirePower}");
+    }
+
+}
+public class Fighter : ISpaceship
+{
+    public int Speed { get;  } = 100;
+    public int FirePower { get;  } = 50;
+
+    public void MoveForward()
+    {
+        System.Console.WriteLine($"Истребитель мчиться впред! его скорость: {Speed}");
+    }
+    public void Rotate(int angle)
+    {
+        System.Console.WriteLine($"Истребитель поворачивается на {angle} градусов!");
+    }
+    public void Fire()
+    {
+        System.Console.WriteLine($"Истребитель атакует! мощность его выстрела: {FirePower}");
+    }
 
 }
--- task04tests/UnitTest1.cs
+++ task04tests/UnitTest1.cs
@@ -1,10 +1,40 @@
 namespace task04tests;
 
-public class UnitTest1
+using Xunit;
+using Moq;
+using task04;
+
+public class SpaceshipTests
 {
     [Fact]
-    public void Test1()
+    public void Cruiser_ShouldHaveCorrectStats()
     {
+        ISpaceship cruiser = new Cruiser();
+        Assert.Equal(50, cruiser.Speed);
+        Assert.Equal(100, cruiser.FirePower);
+    }
 
+    [Fact]
+    public void Fighter_ShouldHaveCorrectStats()
+    {
+        ISpaceship fighter = new Fighter();
+        Assert.Equal(100, fighter.Speed);
+        Assert.Equal(50, fighter.FirePower);
+    }
+
+    [Fact]
+    public void Fighter_ShouldBeFasterThanCruiser()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        Assert.True(fighter.Speed > cruiser.Speed);
+    }
+
+    [Fact]
+    public void Fighter_ShouldBeWeakerThanCruiser()
+    {
+        var fighter = new Fighter();
+        var cruiser = new Cruiser();
+        Assert.True(fighter.FirePower < cruiser.FirePower);
     }
 }
