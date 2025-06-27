a8021928fdcd9e7c4b46b3c22f5eb04a1e882eb6
Vrazzx <ivan.vraz@gmail.com>
2025-06-27 15:31:51 +0600

    Написана реализация заданных методов. дописаны тесты для них. на данном этапе пришлось немного изменить структуру файла с тестами

--- task02tests/UnitTest1.cs
+++ task02tests/UnitTest1.cs
@@ -1,10 +1,69 @@
+using task02;
+using Xunit;
+
 namespace task02tests;
 
-public class UnitTest1
+public class StudentServiceTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private List<Student> _testStudents;
+    private StudentService _service;
 
+    public StudentServiceTests()
+    {
+        _testStudents = new List<Student>
+        {
+            new() { Name = "Иван", Faculty = "ФИТ", Grades = new List<int> { 5, 4, 5 } },
+            new() { Name = "Анна", Faculty = "ФИТ", Grades = new List<int> { 3, 4, 3 } },
+            new() { Name = "Петр", Faculty = "Экономика", Grades = new List<int> { 5, 5, 5 } }
+        };
+        _service = new StudentService(_testStudents);
+    }
+
+    [Fact]
+    public void GetStudentsByFaculty_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsByFaculty("ФИТ").ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Faculty == "ФИТ"));
+    }
+
+    [Fact]
+    public void GetStudentsWithMinAverageGrade_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsWithMinAverageGrade(4.5).ToList();
+        Assert.Equal(2, result.Count);
+        Assert.True(result.All(s => s.Grades.Average() >= 4.5));
+    }
+    [Fact]
+    public void GetStudentsOrderedByName_ReturnsCorrectStudents()
+    {
+        var result = _service.GetStudentsOrderedByName().ToList();
+        Assert.Equal(3, result.Count);
+        Assert.Equal("Анна", result[0].Name);
+        Assert.Equal("Иван", result[1].Name);
+        Assert.Equal("Петр", result[2].Name);
+        
+    }
+
+    [Fact]
+    public void GroupStudentsByFaculty_ReturnsCorrectFaculty()
+    {
+        var result = _service.GroupStudentsByFaculty();
+        Assert.Equal(2, result.Count);
+
+        var fitStudents = result["ФИТ"].ToList();
+        Assert.Equal(2, fitStudents.Count);
+        Assert.True(fitStudents.All(s => s.Faculty == "ФИТ"));
+
+        var econStudents = result["Экономика"].ToList();
+        Assert.Single(econStudents);
+        Assert.True(econStudents.All(s => s.Faculty == "Экономика"));
+    }
+
+    [Fact]
+    public void GetFacultyWithHighestAverageGrade_ReturnsCorrectFaculty()
+    {
+        var result = _service.GetFacultyWithHighestAverageGrade();
+        Assert.Equal("Экономика", result);
     }
 }
