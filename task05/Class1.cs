bccca484a6b03ff7feabfe1937d5df32e2ba4a82
Vrazzx <ivan.vraz@gmail.com>
2025-06-27 23:05:25 +0600

    Реализованы все методы, добавлены тесты, все работает корректно

---
+++ task05/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace task05;
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+    {
+        return _type.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public).Select(m => m.Name);
+    }
+
+    public IEnumerable<string> GetMethodsParams(string methodName)
+    {
+        var method = _type.GetMethods().Where(m => m.Name == methodName);
+        
+        return method.First().GetParameters().Select(c => c.Name ?? string.Empty);
+    }
+
+    public IEnumerable<string> GetAllFields()
+    {
+        return _type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic).Select(f => f.Name);
+    }
+
+    public IEnumerable<string> GetProperties()
+    {
+        return _type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static).Select(p => p.Name);
+    }
+
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return _type.GetCustomAttributes(typeof(T), false).Any();
+    }
+}
