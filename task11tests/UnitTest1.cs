f014c39067548f29d2d391ed794f720df59e21f1
Vrazzx <ivan.vraz@gmail.com>
2025-07-14 18:41:16 +0600

    Реализован динамический калькулятор. написаны к нему тесты, вызов методов происходит без использования рефлексии.

--- task11tests/UnitTest1.cs
+++ task11tests/UnitTest1.cs
@@ -1,10 +1,43 @@
 namespace task11tests;
 
-public class UnitTest1
+using Xunit;
+using task11;
+
+public class DynamicCalculatorTests
 {
     [Fact]
-    public void Test1()
+    public void TestAdd()
     {
+        Type calculatorType = DynamicClassGenerator.GenerateCalculatorType();
+        dynamic calculator = Activator.CreateInstance(calculatorType);
+        
+        Assert.Equal(8, calculator.Add(5, 3));
+    }
 
+    [Fact]
+    public void TestMinus()
+    {
+        Type calculatorType = DynamicClassGenerator.GenerateCalculatorType();
+        dynamic calculator = Activator.CreateInstance(calculatorType);
+        
+        Assert.Equal(2, calculator.Minus(5, 3));
+    }
+
+    [Fact]
+    public void TestMul()
+    {
+        Type calculatorType = DynamicClassGenerator.GenerateCalculatorType();
+        dynamic calculator = Activator.CreateInstance(calculatorType);
+        
+        Assert.Equal(15, calculator.Mul(5, 3));
+    }
+
+    [Fact]
+    public void TestDiv()
+    {
+        Type calculatorType = DynamicClassGenerator.GenerateCalculatorType();
+        dynamic calculator = Activator.CreateInstance(calculatorType);
+        
+        Assert.Equal(2, calculator.Div(6, 3));
     }
 }
