b4b1c40cdd61fad43dbfcf335e75e37f42213ea1
Vrazzx <ivan.vraz@gmail.com>
2025-07-17 21:06:41 +0600

    Реализован интеграл, написаны тесты. Данный изначально ошибочный тест был исправлен

--- task14/Class1.cs
+++ task14/Class1.cs
@@ -1,6 +1,59 @@
-﻿namespace task14;
+﻿using System;
+using System.Threading;
 
-public class Class1
+public class DefiniteIntegral
 {
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsNumber)
+    {
+        if (threadsNumber <= 0) throw new ArgumentException("Число потоков должно быть положительным");
+        if (step <= 0) throw new ArgumentException("Шаг должен быть положительным");
+        if (b < a) throw new ArgumentException("b Должно быть больше a");
 
+        double totalLength = b - a;
+        double segmentLength = totalLength / threadsNumber;
+        double result = 0.0;
+
+
+        using (var barrier = new Barrier(threadsNumber + 1))
+        {
+            for (int i = 0; i < threadsNumber; i++)
+            {
+                double start = a + i * segmentLength;
+                double end = (i == threadsNumber - 1) ? b : start + segmentLength;
+
+                Thread thread = new Thread(() =>
+                {
+                    double partialResult = CalculatePartialIntegral(start, end, function, step);
+                    Interlocked.Exchange(ref result, result + partialResult);
+                    barrier.SignalAndWait();
+                });
+
+                thread.Start();
+            }
+
+
+            barrier.SignalAndWait();
+        }
+
+        return result;
+    }
+
+    private static double CalculatePartialIntegral(double a, double b, Func<double, double> function, double step)
+    {
+        double integral = 0.0;
+        double x = a;
+        double nextX = x + step;
+
+        while (x < b)
+        {
+            if (nextX > b) nextX = b;
+            double y1 = function(x);
+            double y2 = function(nextX);
+            integral += (y1 + y2) * (nextX - x) / 2;
+            x = nextX;
+            nextX += step;
+        }
+
+        return integral;
+    }
 }
--- task14tests/UnitTest1.cs
+++ task14tests/UnitTest1.cs
@@ -1,10 +1,36 @@
 namespace task14tests;
 
-public class UnitTest1
+using Xunit;
+public class DefiniteIntegralTests
 {
-    [Fact]
-    public void Test1()
-    {
 
+
+
+    [Fact]
+    public void LinearFunction_SymmetricInterval_ReturnsZero()
+    {
+        var X = (double x) => x;
+        var result = DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2);
+        Assert.Equal(0, result, 1e-4);
+    }
+    [Fact]
+    public void SinFunction_SymmetricInterval_ReturnsZero()
+    {
+        var SIN = (double x) => Math.Sin(x);
+        var result = DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8);
+        Assert.Equal(0, result, 1e-4);
+    }
+    [Fact]
+    public void LinearFunction_ZeroToFive_ReturnsTen()
+    {
+        var X = (double x) => x;
+        var result = DefiniteIntegral.Solve(0, 5, X, 1e-6, 8);
+        Assert.Equal(12.5, result, 1e-5);
     }
 }
+
+
+
+
+
+
